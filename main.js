7250279ad750d65d43a2e63a14a2f43680a99b45
Erin <erinedawson@gmail.com>
2019-06-09 17:35:51 -0600

    add body to a,m,d setters

--- main.js
+++ main.js
@@ -8,13 +8,19 @@ const menu = {
   get appetizers(){
     return this._courses.appetizers;
   },
-  set appetizers(appIn){},
+  set appetizers(appIn){
+    this.appetizers.push(appIn);
+  },
   get mains(){
     return this._courses.mains;
   },
-  set mains(mainIn){},
+  set mains(mainIn){
+    this.mains.push(mainIn);
+  },
   get desserts(){
     return this.courses.desserts;
   },
-  set desserts(dessIn){}
+  set desserts(dessIn){
+    this.desserts.push(dessIn);
+  }
 };
