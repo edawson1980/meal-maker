30056115f564ad2e3035e3e33013a083bb5f0fa4
Erin <erinedawson@gmail.com>
2019-06-09 18:09:45 -0600

    addDishToCourse method. write test

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ const menu = {
     mains: [],
     desserts: []
   },
+  //efficient alternative to calling indiv setter methods
+  addDishToCourse(courseName, dishName, dishPrice){
+    const dish = {
+      name: dishName,
+      price: dishPrice
+    }
+    this._courses[courseName].push(dish); //assigns the new dish object to the appropriate category: app, main, or dessert
+  },
   get courses(){
     //this method returns an object that contains the key:value pairs for apps, mains, and desserts
     return {
@@ -31,3 +39,5 @@ const menu = {
     this.desserts.push(dessIn);
   }
 };
+menu.addDishToCourse('appetizers', 'fries', 2.50);
+console.log(menu.appetizers);
