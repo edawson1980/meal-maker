4b533786e1ed307f32ea928218f89a6cb6285220
Erin <erinedawson@gmail.com>
2019-06-09 18:31:50 -0600

    add getRandomDishFromCourse method to menu object

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ const menu = {
     desserts: []
   },
   //efficient alternative to calling indiv setter methods
-  addDishToCourse(courseName, dishName, dishPrice){
+  addDishToCourse: function(courseName, dishName, dishPrice){
     const dish = {
       name: dishName,
       price: dishPrice
     }
     this._courses[courseName].push(dish); //assigns the new dish object to the appropriate category: app, main, or dessert
+  },
+  getRandomDishFromCourse: function(courseName){
+    const dishes = this._courses[courseName]; //get the array of the course named
+    const randIndex = Math.floor(Math.random()*dishes.length);
+    return dishes[randIndex];
   },
   get courses(){
     //this method returns an object that contains the key:value pairs for apps, mains, and desserts
@@ -39,5 +44,5 @@ const menu = {
     this.desserts.push(dessIn);
   }
 };
-menu.addDishToCourse('appetizers', 'fries', 2.50);
-console.log(menu.appetizers);
+// menu.addDishToCourse('appetizers', 'mozz sticks', 4.50);
+// console.log(menu.appetizers);
